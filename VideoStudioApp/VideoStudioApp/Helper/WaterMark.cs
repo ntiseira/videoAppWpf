ce3687eb375cd4652a8b83f767161fa92b2ae50b
ntiseira <nicolas_tiseir@hotmail.com>
2015-11-20 17:57:04 -0300

    Se agrego prueba de marca de agua con libreria nreco

---
+++ VideoStudioApp/VideoStudioApp/Helper/WaterMark.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStudioApp.Helper
+{
+   public static class WaterMark
+    {
+         /// <summary>
+ /// This method will create a bitmap based 
+ /// </summary>
+ /// <param name="overlayText"></param>
+ /// <param name="rootPath"></param>
+ /// <param name="width"></param>
+  /// <param name="height"></param>
+  /// <returns></returns>
+ public static string createOverlayImage(string overlayText, string rootPath, int width, int height)
+ {
+     // full path for a temporary bitmap
+     string overlayFileName = rootPath + "\\" + Guid.NewGuid().ToString() + ".bmp";
+   
+     // create a bitmap
+     Bitmap bitmap = new Bitmap(width, height);
+     Graphics g = Graphics.FromImage(bitmap);
+   
+     // define the font
+     Font font = new Font("Arial", (float)14.0);
+   
+     // define the area to draw on
+     Rectangle area = new Rectangle(new Point(0, 0), new Size(width, height));
+   
+// draw the new image
+g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+g.DrawString(overlayText, font, Brushes.Red, area);
+     // save the picture with the text overlay
+     bitmap.Save(overlayFileName);
+// return the path to the overlay image
+return overlayFileName;
+ }
+
+
+    }
+}
--- VideoStudioApp/VideoStudioApp/ViewModel/MainWindowViewModel.cs
+++ VideoStudioApp/VideoStudioApp/ViewModel/MainWindowViewModel.cs
@@ -5,8 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Microsoft.Expression.Encoder;
 using VideoStudioApp.Command;
+using VideoStudioApp.Helper;
 using VideoStudioApp.Views;
+using System.Drawing;
+using NReco.VideoConverter;
+
 
 namespace VideoStudioApp.ViewModel
 {
@@ -19,9 +24,28 @@ namespace VideoStudioApp.ViewModel
         public MainWindowViewModel(Window home)
         {
             //CurrentWindow = window;
-            Home = home;         
+            Home = home;
+            Prueba();
         }
 
+        public void Prueba()
+        {
+
+
+
+// NReco.VideoConverter.FFMpegConverter wrap = new FFMpegConverter();
+ //wrap.Invoke("-i c:\\test.avi -i C:\\prueba.bmp -filter_complex \"overlay=10:10\" c:\\test2.avi");
+
+ var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+  //Convierte de formato
+            //  ffMpeg.ConvertMedia("c:\\test.avi", "video.mp4", Format.mp4);
+
+    ffMpeg.Invoke("-i c:\\video.mp4 -i C:\\prueba.bmp -filter_complex \"overlay=10:30\" -codec:a copy videova.mp4");
+
+// ffMpeg.Invoke("-i c:\\video.mp4 -i C:\\prueba.bmp -filter_complex \"[0:v][1:v]overlay=main_w-overlay_w-10:10\" -codec:a copy videoaa.mp4");
+          
+
+        }
 
         private ICommand iniciarAdminCommand;
         public ICommand IniciarAdminCommand
